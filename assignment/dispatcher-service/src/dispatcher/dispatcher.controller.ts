ee4453bf61e20b5769428cb161d05ae7dce0b79c
Dinalidh <dinalihettiarachchi96@gmail.com>
2025-03-04 16:28:01 +0530

    Assignment: Retrieve Vehicles by City

--- assignment/dispatcher-service/src/dispatcher/dispatcher.controller.ts
+++ assignment/dispatcher-service/src/dispatcher/dispatcher.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Param, Get } from '@nestjs/common';
 import { DispatcherService } from './dispatcher.service';
 import { CreateDispatcherDto } from 'src/dto/create-dispatcher.dto';
 import { Dispatcher } from 'src/entity/dispatcher.entity';
@@ -12,4 +12,12 @@ export class DispatcherController {
         @Body() CreateDispatcherDto: CreateDispatcherDto,): Promise<Dispatcher> {
         return this.dispatcherService.createDispatcher(CreateDispatcherDto)
     }
+
+    @Get(':city')
+    async getProductByCity(@Param('city') city: string): Promise<Dispatcher> {
+        return this.dispatcherService.getDispatcherBycity(city);
+    }
+
+
+
 }
--- assignment/dispatcher-service/src/dispatcher/dispatcher.service.ts
+++ assignment/dispatcher-service/src/dispatcher/dispatcher.service.ts
@@ -2,6 +2,7 @@ import { CreateDispatcherDto } from "src/dto/create-dispatcher.dto";
 import { Dispatcher } from "src/entity/dispatcher.entity";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {NotFoundException} from '@nestjs/common';
 
 export class DispatcherService {
     constructor(
@@ -12,6 +13,14 @@ export class DispatcherService {
     async createDispatcher(CreateDispatcherDto: CreateDispatcherDto): Promise<Dispatcher> {
         const dispatcher = this.dispatcherRepository.create(CreateDispatcherDto);
         return this.dispatcherRepository.save(dispatcher);
+    }
+
+    async getDispatcherBycity(city: string): Promise<Dispatcher> {
+        const dispatcher = await this.dispatcherRepository.findOne({ where: { city } });
+        if (!dispatcher) {
+            throw new NotFoundException(`Dispatcher with City ${city} not found`);
+        }
+        return dispatcher;
     }
 
 }
